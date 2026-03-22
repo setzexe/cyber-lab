bbc733b1f0b1e49de8babc69ba985238c3057b39
setzexe <ryantpsetzer@outlook.com>
2026-03-22 10:47:01 -0400

    Add signature to access.sh

--- shell-script/access.sh
+++ shell-script/access.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# This script opens the current directory (or a chosen directory) in VSC.
+# Name: access.sh 
+# Usage: ./access.sh
+# Creator: cyberseverance (a.k.a. setz)
+
 cd "your/directory/here" # Change this to the directory you want to access.
 code .
